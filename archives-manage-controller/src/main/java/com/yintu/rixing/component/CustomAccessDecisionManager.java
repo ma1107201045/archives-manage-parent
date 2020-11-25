9425ddc8e70efc24e8995952bdbcde9e7e86f70e
global_mwy <global_1107201045@qq.com>
2020-11-25 17:51:35 +0800

    edit CustomAccessDecisionManager.java

--- archives-manage-controller/src/main/java/com/yintu/rixing/component/CustomAccessDecisionManager.java
+++ archives-manage-controller/src/main/java/com/yintu/rixing/component/CustomAccessDecisionManager.java
@@ -2,7 +2,10 @@ package com.yintu.rixing.component;
 
 import org.springframework.security.access.AccessDecisionManager;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.access.AuthorizationServiceException;
 import org.springframework.security.access.ConfigAttribute;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
@@ -22,7 +25,14 @@ public class CustomAccessDecisionManager implements AccessDecisionManager {
 
     @Override
     public void decide(Authentication authentication, Object object, Collection<ConfigAttribute> configAttributes) throws AccessDeniedException, InsufficientAuthenticationException {
+        System.out.println(authentication);
         System.out.println(authentication.getPrincipal());
+        if (authentication instanceof AnonymousAuthenticationToken) {
+            throw new AuthenticationServiceException("尚未登录，请先登录");
+        } else {
+            throw new AuthorizationServiceException("权限不足，请联系管理员");
+        }
+
     }
 
     @Override
