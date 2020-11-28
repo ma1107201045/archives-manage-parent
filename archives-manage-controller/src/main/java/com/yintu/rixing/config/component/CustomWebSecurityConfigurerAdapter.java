643b3b09dc1a93bd7d94d1d582bf5f7b8c235633
global_mwy <global_1107201045@qq.com>
2020-11-28 19:50:29 +0800

    edit CustomSwaggerApiDescription.java      CustomWebSecurityConfigurerAdapter.java files

--- archives-manage-controller/src/main/java/com/yintu/rixing/config/component/CustomWebSecurityConfigurerAdapter.java
+++ archives-manage-controller/src/main/java/com/yintu/rixing/config/component/CustomWebSecurityConfigurerAdapter.java
@@ -123,16 +123,16 @@ public class CustomWebSecurityConfigurerAdapter extends WebSecurityConfigurerAda
             out.close();
         })
                 .and().exceptionHandling()
-                .authenticationEntryPoint((request, response, authenticationException) -> { //没有登录权限时，在这里处理结果，不要重定向
-                    response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
-                    response.setStatus(HttpServletResponse.SC_OK);
-                    PrintWriter out = response.getWriter();
-                    Map<String, Object> errorData = ResponseDataUtil.noLogin(authenticationException.getMessage());
-                    JSONObject jo = (JSONObject) JSONObject.toJSON(errorData);
-                    out.write(jo.toJSONString());
-                    out.flush();
-                    out.close();
-                })
+//                .authenticationEntryPoint((request, response, authenticationException) -> { //没有登录权限时，在这里处理结果，不要重定向
+//                    response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+//                    response.setStatus(HttpServletResponse.SC_OK);
+//                    PrintWriter out = response.getWriter();
+//                    Map<String, Object> errorData = ResponseDataUtil.noLogin(authenticationException.getMessage());
+//                    JSONObject jo = (JSONObject) JSONObject.toJSON(errorData);
+//                    out.write(jo.toJSONString());
+//                    out.flush();
+//                    out.close();
+//                })
                 .accessDeniedHandler((request, response, accessDeniedException) -> {  //没有访问权限时，在这里处理结果，不要重定向
                     response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
                     response.setStatus(HttpServletResponse.SC_OK);
