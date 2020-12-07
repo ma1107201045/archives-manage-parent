9aa6abcd41b9b5799d8193da4f01ba09dbc4e898
global_mwy <global_1107201045@qq.com>
2020-12-07 19:37:02 +0800

    edit ArchivesManageApplication.java      pom.xml      pom.xml files

--- archives-manage-controller/src/main/java/com/yintu/rixing/ArchivesManageApplication.java
+++ archives-manage-controller/src/main/java/com/yintu/rixing/ArchivesManageApplication.java
@@ -4,7 +4,6 @@ import com.github.xiaoymin.swaggerbootstrapui.annotations.EnableSwaggerBootstrap
 import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2Doc
 @EnableSwaggerBootstrapUI
