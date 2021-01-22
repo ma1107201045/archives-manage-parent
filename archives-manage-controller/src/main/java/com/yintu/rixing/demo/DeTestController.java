e6c3e86c3b3a5782aae090bd9063c94d140a20a5
global_mwy <global_1107201045@qq.com>
2021-01-22 09:47:10 +0800

    edit too much files

--- archives-manage-controller/src/main/java/com/yintu/rixing/demo/DeTestController.java
+++ archives-manage-controller/src/main/java/com/yintu/rixing/demo/DeTestController.java
@@ -5,8 +5,10 @@ import com.yintu.rixing.system.SysUser;
 import com.yintu.rixing.util.ResultDataUtil;
 import com.yintu.rixing.util.ResponseDataUtil;
 import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
 /**
@@ -20,6 +22,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/demo/de-test")
 @Api(tags = "测试接口")
+@Slf4j
 public class DeTestController {
 
     @GetMapping("/test")
@@ -34,6 +37,23 @@ public class DeTestController {
 
     @GetMapping("/test2/{archivesId}")
     public ResultDataUtil<Map<String, Object>> test2(@PathVariable Integer archivesId, @RequestParam Map<String, Object> params) {
+        return ResultDataUtil.ok("", null);
+    }
+
+    @GetMapping("/test3")
+    public ResultDataUtil<Map<String, Object>> test3(HttpServletRequest request) {
+        log.debug("getRemoteHost:" + request.getRemoteHost());
+        log.debug("getRemotePort:" + request.getRemotePort());
+        log.debug("getRemoteAddr:" + request.getRemoteAddr());
+        log.debug("getRemoteUser:" + request.getRemoteUser());
+        log.debug("-------------------------------------");
+        log.debug("getScheme:" + request.getScheme());
+        log.debug("getServerName:" + request.getServerName());
+        log.debug("getServerPort:" + request.getServerPort());
+        log.debug("getRequestURL:" + request.getRequestURL());
+        log.debug("getRequestURI:" + request.getRequestURI());
+        log.debug("isSecure:" + request.isSecure());
+        log.debug("-------------------------------------");
         return ResultDataUtil.ok("", null);
     }
 }
