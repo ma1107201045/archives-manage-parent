4e9509533c243198795918e13972c6632dd70a39
global_mwy <global_1107201045@qq.com>
2021-01-29 15:26:18 +0800

    edit JwtTokenUtil.java file

--- archives-manage-controller/src/main/java/com/yintu/rixing/config/jwt/JwtTokenUtil.java
+++ archives-manage-controller/src/main/java/com/yintu/rixing/config/jwt/JwtTokenUtil.java
@@ -12,6 +12,7 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import java.util.Date;
 import java.util.Objects;
@@ -105,5 +106,6 @@ public class JwtTokenUtil {
     public String parseJWTSignature(String token) {
         return parseJWT(token).getSignature();
     }
+
 
 }
