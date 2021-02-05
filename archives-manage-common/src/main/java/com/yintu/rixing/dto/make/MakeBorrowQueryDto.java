e9d0b26a29fde3160da617d07645579b56e8472d
global_mwy <global_1107201045@qq.com>
2021-02-05 20:44:05 +0800

    edit MakeBorrowQueryDto.java file

--- archives-manage-common/src/main/java/com/yintu/rixing/dto/make/MakeBorrowQueryDto.java
+++ archives-manage-common/src/main/java/com/yintu/rixing/dto/make/MakeBorrowQueryDto.java
@@ -16,11 +16,11 @@ import lombok.EqualsAndHashCode;
 @ApiModel
 public class MakeBorrowQueryDto extends PageDto {
 
-    @ApiModelProperty(value = "借阅人id", hidden = true)
-    private Integer userId;
-
     @ApiModelProperty(value = "内部和远程人员区分 1：内部 2：远程", hidden = true)
     private Short userType;
+
+    @ApiModelProperty(value = "借阅人id", hidden = true)
+    private Integer userId;
 
     @ApiModelProperty(value = "借阅类型 1：电子借阅 2：实体借阅", hidden = true)
     private Short borrowType;
