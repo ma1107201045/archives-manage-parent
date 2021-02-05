9c8c502cd12ac6d0875d18816aebfb0a28cd28d7
global_mwy <global_1107201045@qq.com>
2021-02-05 17:20:13 +0800

    edit IPerBorrowManagementService.java      PerBorrowManagementController.java      PerBorrowManagementInfoVo.java      PerBorrowManagementQueryDto.java      PerBorrowManagementServiceImpl.java      PerBorrowManagementVo.java      pom.xml      pom.xml      SwaggerConfiguration.java files

---
+++ archives-manage-service/src/main/java/com/yintu/rixing/person/IPerBorrowManagementService.java
@@ -0,0 +1,11 @@
+package com.yintu.rixing.person;
+
+/**
+ * @Author: mlf
+ * @Date: 2021/2/5 16:41:24
+ * @Version: 1.0
+ */
+public interface IPerBorrowManagementService {
+
+
+}
---
+++ archives-manage-service/src/main/java/com/yintu/rixing/person/impl/PerBorrowManagementServiceImpl.java
@@ -0,0 +1,13 @@
+package com.yintu.rixing.person.impl;
+
+import com.yintu.rixing.person.IPerBorrowManagementService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author: mlf
+ * @Date: 2021/2/5 16:41:46
+ * @Version: 1.0
+ */
+@Service
+public class PerBorrowManagementServiceImpl implements IPerBorrowManagementService {
+}
