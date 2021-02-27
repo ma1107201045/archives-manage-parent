a267829c415d46f2776846799413c39252199a2b
global_mwy <global_1107201045@qq.com>
2021-02-27 18:23:02 +0800

    edit CaptchaController.java file

--- archives-manage-controller/src/main/java/com/yintu/rixing/config/controller/CaptchaController.java
+++ archives-manage-controller/src/main/java/com/yintu/rixing/config/controller/CaptchaController.java
@@ -2,6 +2,8 @@ package com.yintu.rixing.config.controller;
 
 import cn.hutool.captcha.CaptchaUtil;
 import cn.hutool.captcha.CircleCaptcha;
+import cn.hutool.captcha.ShearCaptcha;
+import cn.hutool.captcha.generator.MathGenerator;
 import com.yintu.rixing.annotation.Log;
 import com.yintu.rixing.enumobject.EnumLogLevel;
 import io.swagger.annotations.Api;
@@ -40,4 +42,6 @@ public class CaptchaController {
         os.flush();
         os.close();
     }
+
+
 }
