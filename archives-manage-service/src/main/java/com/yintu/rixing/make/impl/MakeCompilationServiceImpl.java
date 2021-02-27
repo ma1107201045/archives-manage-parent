7cf2622fed3ebc0bfff2c0ededbe2111bdc85bf5
111 <111>
2021-02-27 19:11:37 +0800

    库房管理

--- archives-manage-service/src/main/java/com/yintu/rixing/make/impl/MakeCompilationServiceImpl.java
+++ archives-manage-service/src/main/java/com/yintu/rixing/make/impl/MakeCompilationServiceImpl.java
@@ -1,11 +1,15 @@
 package com.yintu.rixing.make.impl;
 
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.yintu.rixing.make.IMakeCompilationService;
 import com.yintu.rixing.make.MakeCompilation;
 import com.yintu.rixing.make.MakeCompilationMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
@@ -17,5 +21,33 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class MakeCompilationServiceImpl extends ServiceImpl<MakeCompilationMapper, MakeCompilation> implements IMakeCompilationService {
+    @Autowired
+    private MakeCompilationMapper makeCompilationMapper;
 
+    @Override
+    public Page<MakeCompilation> findAllMyCompilation(Integer num, Integer size, String topicName, Integer userId) {
+        Page page=new Page();
+        page.setSize(size);
+        page.setCurrent(num);
+        Page<MakeCompilation> makeCompilationPage=makeCompilationMapper.findAllMyCompilation(page,topicName,userId);
+        return makeCompilationPage;
+    }
+
+    @Override
+    public Page<MakeCompilation> findAllAlreadyAudit(Integer num, Integer size, String topicName, Integer userId) {
+        Page page=new Page();
+        page.setSize(size);
+        page.setCurrent(num);
+        Page<MakeCompilation> makeCompilationPage=makeCompilationMapper.findAllAlreadyAudit(page,topicName,userId);
+        return makeCompilationPage;
+    }
+
+    @Override
+    public Page<MakeCompilation> findAllNotAudit(Integer num, Integer size, String topicName, Integer userId) {
+        Page page=new Page();
+        page.setSize(size);
+        page.setCurrent(num);
+        Page<MakeCompilation> makeCompilationPage=makeCompilationMapper.findAllNotAudit(page,topicName,userId);
+        return makeCompilationPage;
+    }
 }
