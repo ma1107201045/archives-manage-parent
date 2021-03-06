839e0f2abf1ad4834bbbee623f65bceee70cf73d
global_mwy <global_1107201045@qq.com>
2021-03-06 14:48:39 +0800

    edit VerificationCodeFilter.java file

--- archives-manage-controller/src/main/java/com/yintu/rixing/config/filter/VerificationCodeFilter.java
+++ archives-manage-controller/src/main/java/com/yintu/rixing/config/filter/VerificationCodeFilter.java
@@ -25,7 +25,6 @@ import java.util.Map;
 @Component
 public class VerificationCodeFilter extends OncePerRequestFilter {
 
-
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         //判断请求页面是否为/login、该路径对应登录form表单的action路径，请求的方法是否为POST，是的话进行验证码校验逻辑，否则直接执行filterChain.doFilter让代码往下走
@@ -61,16 +60,16 @@ public class VerificationCodeFilter extends OncePerRequestFilter {
 
         //验证码空校验
         if (StrUtil.isBlank(codeInRequest)) {
-            throw new VerificationCodeException("验证码不能为空！");
+            throw new VerificationCodeException("验证码不能为空");
         }
 
         //验证码校验
         if (codeInSession == null) {
-            throw new VerificationCodeException("验证码不存在，请重新发送！");
+            throw new VerificationCodeException("验证码不存在，请重新发送");
         }
         //判断是否相等
         if (!StrUtil.equalsAnyIgnoreCase(codeInRequest, codeInSession)) {
-            throw new VerificationCodeException("验证码不正确！");
+            throw new VerificationCodeException("验证码不正确");
         }
         //从Session移除该字段信息
         request.getSession().removeAttribute("captcha");
